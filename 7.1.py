3c56a84f4d8a9eeaf3d89c7a9f033504b93ad769
Varun Sonavni <varun.sonavni@converj.com>
2023-09-28 13:57:58 +0530

    update 7.1.py

--- 7.1.py
+++ 7.1.py
@@ -2,6 +2,8 @@ import random
 
 import os
 
+import wordlist
+
 hangman_ascii = """
  _   _    _    _   _  ____ __  __    _    _   _
 | | | |  / \  | \ | |/ ___|  \/  |  / \  | \ | |
@@ -11,11 +13,69 @@ hangman_ascii = """
              
 """
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 print(hangman_ascii)
 
 # Words
 
-word_list = ["aarark", "baboon", "camel", "beekeeper"]
+# word_list = ["aarark", "baboon", "camel", "beekeeper"]
+word_list = wordlist.word_list
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -25,7 +85,8 @@ ans = []
 for i in chosen_word:
     ans.append("_")
 
-chance = 7
+chance = 6
+flag = False
 
 while chance > 0:
     guess = input("\nGuess a letter: ").lower()
@@ -35,23 +96,24 @@ while chance > 0:
         
         if chosen_word[letter] == guess:
             ans[letter] = guess
-        else:
-            os.system("clear")
-            print("print")
+            flag = True
 
-        # elif chosen_word[letter] != guess:
-        #     pass
-        #     print("print")
-        
-            
-
-    
-    # os.system("clear")
     print(ans)
+    print(stages[chance])    
+
     if "_" not in ans:
         print("You Win!")
         break 
-    chance -= 1
+     
+    if guess not in chosen_word:
+        os.system("clear")
+        print(f"You guessed {guess}, that's not in the word. You lose a life. Total Remaining life {chance}\n")
+        chance -= 1
+        print(stages[chance])
+
+
+
+
 
 if "_" in ans:
     print("You Lose!")
