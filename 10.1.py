1684a92003cec8529e541e3dedecb6eb8bb335ab
Varun Sonavni <varun.sonavni@converj.com>
2023-10-06 15:00:34 +0530

    update 10.1

--- 10.1.py
+++ 10.1.py
@@ -13,16 +13,16 @@ def is_leap(year):
 def days_in_month(year, month):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   leap_year = is_leap(year)
-  print(leap_year)
+#   print(leap_year)
   month -= 1
   if leap_year:
     if month_days[month] == month_days[1]:
-      print(month_days[month] + 1)
-      return
+      return(month_days[month] + 1)
+      
         
-    print(month_days[month])
+    return(month_days[month])
   else:
-    print(month_days[month])
+    return(month_days[month])
 
 
   
@@ -31,7 +31,7 @@ def days_in_month(year, month):
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
-# print(days)
+print(days)
 
 
 
