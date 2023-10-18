74b2a351d5fa25b0fe955129990d7e2deda38af5
Varun Sonavni <varun.sonavni@converj.com>
2023-10-18 18:48:09 +0530

    add 19 day

---
+++ 19.1.py
@@ -0,0 +1,45 @@
+import turtle as t
+import os
+
+timmy = t.Turtle()
+
+screen = t.Screen()
+
+t.title("Keyboard Game...")
+t.bgcolor("white")
+
+def move_forward():
+    timmy.forward(20)
+
+def move_backward():
+    timmy.backward(20)
+
+def move_clockwise():
+    timmy.right(20)
+
+def move_anticlockwise():
+    timmy.left(20)
+
+def clear():
+    timmy.clear()
+    timmy.penup()
+    timmy.home()
+
+screen.listen()  
+screen.onkeypress(key="w", fun=move_forward)
+screen.onkey(key="w", fun=move_forward) 
+
+screen.onkey(key="s", fun=move_backward) 
+screen.onkeypress(key="s", fun=move_backward)
+
+screen.onkey(key="a", fun=move_anticlockwise) 
+screen.onkeypress(key="a", fun=move_anticlockwise)
+
+
+screen.onkey(key="d", fun=move_clockwise) 
+screen.onkeypress(key="d", fun=move_clockwise)
+
+screen.onkey(key="c", fun=clear) 
+screen.onkeypress(key="c", fun=clear)
+
+screen.exitonclick()
